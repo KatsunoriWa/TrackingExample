1baae24df44577cafed05034d701184ba2e1f099
papa <gtx285aa@yahoo.co.jp>
2017-11-18 20:02:07 +0900

    preparing for multi face

--- tracker.py
+++ tracker.py
@@ -49,52 +49,54 @@ if __name__ == '__main__':
     largest = largestRect(rects)
     print largest
 
-    bbox = tuple(largest)
 
 
     tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
     tracker_type = tracker_types[2]
 
-    if int(minor_ver) < 3:
-        tracker = cv2.Tracker_create(tracker_type)
-    else:
-        if tracker_type == 'BOOSTING':
-            tracker = cv2.TrackerBoosting_create()
-        if tracker_type == 'MIL':
-            tracker = cv2.TrackerMIL_create()
-        if tracker_type == 'KCF':
-            tracker = cv2.TrackerKCF_create()
-        if tracker_type == 'TLD':
-            tracker = cv2.TrackerTLD_create()
-        if tracker_type == 'MEDIANFLOW':
-            tracker = cv2.TrackerMedianFlow_create()
-        if tracker_type == 'GOTURN':
-            tracker = cv2.TrackerGOTURN_create()
 
+    trackers = range(len(rects))
 
-    ok = tracker.init(frame, bbox)
+    for i, rect in enumerate(rects):
+
+        if int(minor_ver) < 3:
+            trackers[i] = cv2.Tracker_create(tracker_type)
+        else:
+            if tracker_type == 'BOOSTING':
+                trackers[i] = cv2.TrackerBoosting_create()
+            if tracker_type == 'MIL':
+                trackers[i] = cv2.TrackerMIL_create()
+            if tracker_type == 'KCF':
+                trackers[i] = cv2.TrackerKCF_create()
+            if tracker_type == 'TLD':
+                trackers[i] = cv2.TrackerTLD_create()
+            if tracker_type == 'MEDIANFLOW':
+                trackers[i] = cv2.TrackerMedianFlow_create()
+            if tracker_type == 'GOTURN':
+                trackers[i] = cv2.TrackerGOTURN_create()
+
+        ok = trackers[i].init(frame, tuple(rects[i]))
 
     while True:
         ok, frame = video.read()
         if not ok:
             break
 
-        timer = cv2.getTickCount()
 
-        ok, bbox = tracker.update(frame)
+        for i, tracker in enumerate(trackers):
 
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+            ok, bbox = tracker.update(frame)
 
-        if ok:            # Tracking success
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-        else:
-            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+            if ok:            # Tracking success
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+            else:
+                cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         cv2.imshow("Tracking", frame)
 
