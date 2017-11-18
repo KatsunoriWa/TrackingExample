14fefc20061bb16f6adc14590ddeac50a8c30a40
papa <gtx285aa@yahoo.co.jp>
2017-11-18 19:50:07 +0900

    added check to xml file.

--- tracker.py
+++ tracker.py
@@ -4,8 +4,15 @@
 """
 https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/
 """
+import sys
+import os
+import cv2
 
 def largestRect(rects):
+    u"""retturn largest rect in rects
+    rects: list of rect
+    """
+
     if len(rects) < 2:
         return rects
 
@@ -16,18 +23,14 @@ def largestRect(rects):
 
     return largest
 
-import cv2
-import sys
-
-(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split(".")
-
 if __name__ == '__main__':
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split(".")
     cascade_path = "haarcascade_frontalface_alt.xml"
+    if not os.path.isfile(cascade_path):
+        print "be sure to get ready for %s" % os.path.basename(cascade_path)
+        sys.exit()
+
     cascade = cv2.CascadeClassifier(cascade_path)
-
-
-    # Set up tracker.
-    # Instead of MIL, you can also use
 
     tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
     tracker_type = tracker_types[2]
@@ -48,28 +51,18 @@ if __name__ == '__main__':
         if tracker_type == 'GOTURN':
             tracker = cv2.TrackerGOTURN_create()
 
-    # Read video
 #    video = cv2.VideoCapture("videos/chaplin.mp4")
     video = cv2.VideoCapture(0)
 
-    # Exit if video not opened.
+
     if not video.isOpened():
         print "Could not open video"
         sys.exit()
 
-    # Read first frame.
     ok, frame = video.read()
     if not ok:
         print 'Cannot read video file'
         sys.exit()
-
-    if 0:
-        # Define an initial bounding box
-        bbox = (287, 23, 86, 320)
-
-        # Uncomment the line below to select a different bounding box
-        bbox = cv2.selectROI(frame, False)
-        print bbox
 
     rects = cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
     largest = largestRect(rects)
@@ -77,43 +70,33 @@ if __name__ == '__main__':
 
     bbox = tuple(largest)
 
-    # Initialize tracker with first frame and bounding box
     ok = tracker.init(frame, bbox)
 
     while True:
-        # Read a new frame
         ok, frame = video.read()
         if not ok:
             break
 
-        # Start timer
         timer = cv2.getTickCount()
 
-        # Update tracker
         ok, bbox = tracker.update(frame)
 
-        # Calculate Frames per second (FPS)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
 
-        # Draw bounding box
-        if ok:
-            # Tracking success
+        if ok:            # Tracking success
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
         else:
-            # Tracking failure
             cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-        # Display tracker type on frame
         cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
-        # Display FPS on frame
         cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
-        # Display result
         cv2.imshow("Tracking", frame)
 
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
-        if k == 27: break
+        if k == 27:
+            break
