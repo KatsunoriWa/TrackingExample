0d85c7a502dd54ec393057335decce9910388067
papa <gtx285aa@yahoo.co.jp>
2017-11-18 19:35:26 +0900

    pylint

--- tracker.py
+++ tracker.py
@@ -8,12 +8,12 @@ https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/
 def largestRect(rects):
     if len(rects) < 2:
         return rects
-    
+
     largest = rects[0]
     for i in range(1, len(rects)):
         if rects[i][2] > largest[2]:
             largest = rects[i]
-    
+
     return largest
 
 import cv2
@@ -21,8 +21,7 @@ import sys
 
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split(".")
 
-if __name__ == '__main__' :
-    
+if __name__ == '__main__':
     cascade_path = "haarcascade_frontalface_alt.xml"
     cascade = cv2.CascadeClassifier(cascade_path)
 
@@ -30,7 +29,7 @@ if __name__ == '__main__' :
     # Set up tracker.
     # Instead of MIL, you can also use
 
-    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
     tracker_type = tracker_types[2]
 
     if int(minor_ver) < 3:
@@ -67,11 +66,11 @@ if __name__ == '__main__' :
     if 0:
         # Define an initial bounding box
         bbox = (287, 23, 86, 320)
-    
+
         # Uncomment the line below to select a different bounding box
         bbox = cv2.selectROI(frame, False)
         print bbox
-    
+
     rects = cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
     largest = largestRect(rects)
     print largest
@@ -101,20 +100,20 @@ if __name__ == '__main__' :
             # Tracking success
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-        else :
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+        else:
             # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+        cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         # Display result
         cv2.imshow("Tracking", frame)
 
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
-        if k == 27 : break
+        if k == 27: break
